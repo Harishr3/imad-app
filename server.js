f4deb1cf4616e72db305fcaeb7a25beb44a6f1d6
Harishr3 <30951760+Harishr3@users.noreply.github.com>
2017-08-14 14:10:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/My new file',function (req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'my new file.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
